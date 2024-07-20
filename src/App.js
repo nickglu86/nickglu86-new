191489a2634ef4b3a164d0ba0c4a72b80291ec69
nickglu86 <enayfls@gmail.com>
2024-07-20 11:25:55 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Lottie from "lottie-react";
+import appAnimation from "./assets/app-animation.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#080808",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+        }}
+      >
+        <div className="content">
+          <h1 className="title">
+            Crafting <span className="gradient-text">Digital Solutions</span> 
+            <br />
+            for the future of tomorrow
+          </h1>
+          <p className="desc">
+            Experience the power of smart note-taking and transform the way you
+            work today.
+          </p>
+          <button>Get Started</button>
+        </div>
+      </div>
+      <div style={{ position: "relative", width: "45%", marginTop: '100px', overflowl: 'hidden' }}>
+        <Lottie animationData={appAnimation} />
+        <div className="watermark-cover"></div>
+      </div>
     </div>
   );
 }
